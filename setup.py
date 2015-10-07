f76abdef77a6e79f1f0a8977e1e726178239b38f
Don Drake <don@drakeconsulting.com>
2015-10-06 22:18:33 -0500

    version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1',
+    version='1.1.0',
 
     description='Enhanced Python Dataframes for Spark/PySpark',
     long_description=open("README.rst").read(),
